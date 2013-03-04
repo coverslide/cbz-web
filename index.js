20031dac99185d05b0ddf7c6a3624726cfd35d51
Richard Hoffman <coverslide@gmail.com>
2013-03-04 14:08:11 -0800

    Use WSFTP instead of filesocket-client

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 var send = require('send')
-var FileSocketServer = require('filesocket-server')
+var WSFTP = require('wsftp')
 
-module.exports = function(root){
-  var files = new FileSocketServer({root: root})
-  return {
-    bind: function(server, path){
-      server.on('request', function(req, res){
-        send(req, req.url)
-          .root(__dirname + '/public')
-          .pipe(res)
-      })
-      var bindOptions = {server: server}
-      if(path) bindOptions.path = path
-      files.bind(bindOptions)
-    }
-  }
+module.exports = function(root, server, path){
+  server.on('request', function(req, res){
+    send(req, req.url)
+      .root(__dirname + '/public')
+      .pipe(res)
+  })
+  var bindOptions = {server: server}
+  if(path) bindOptions.path = path
+  new WSFTP(bindOptions, root)
 }
